ff6d979af9f425e2ca2f36987b310110a697fdd6
koimasshiro <ese.imhariagbe@gmail.com>
2024-03-22 02:31:33 +0100

    fixed error: Modified cors origin on service

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://scissor-urlshortener-project-frontend-krco.onrender.com",
     credentials: true,
   })
 );
